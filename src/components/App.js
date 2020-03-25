eb5b4b9a0547212b03021a9bef04e9131e606992
Anurag Bajaj <anuragb26@gmail.com>
2020-03-25 06:03:17 +0530

    Chore:Add Semantic UI

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "semantic-ui-react";
+
+const App = () => {
+  return (
+    <div>
+      <Button primary>Hi</Button>
+    </div>
+  );
+};
+
+export default App;
