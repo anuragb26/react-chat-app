8ea4ce34231fbac3da9c6963da9729483dbce986
Anurag Bajaj <anuragb26@gmail.com>
2020-03-25 10:12:11 +0530

    Chore:Add Message Input and Send Message Button

--- src/components/ChatPanel.js
+++ src/components/ChatPanel.js
@@ -5,8 +5,21 @@ const ChatPanel = () => {
   return (
     <div className="ChatPanel">
       <div>room1</div>
-      <div style={{ flexGrow: 1, backgroundColor: "violet" }}>message</div>
-      <div>send messages</div>
+      <div style={{ flexGrow: 1, backgroundColor: "violet" }}>
+        <ul>
+          <li>Message 1</li>
+          <li>Message 1</li>
+          <li>Message 1</li>
+        </ul>
+      </div>
+      <div style={{ display: "flex" }}>
+        <input
+          type="text"
+          style={{ flexGrow: 1 }}
+          placeholder="insert message"
+        />
+        <button>Send Message</button>
+      </div>
     </div>
   );
 };
