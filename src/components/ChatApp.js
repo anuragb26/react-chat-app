c73b9ab414b33b4a0def6ea5a7e31fc6b4397245
Anurag Bajaj <anuragb26@gmail.com>
2020-03-26 09:15:22 +0530

    Feat:Wrap Context

--- src/components/ChatApp.js
+++ src/components/ChatApp.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import SidePanel from "./SidePanel/SidePanel";
 import ChatPanel from "./ChatPanel/ChatPanel";
+import UserContext from "../context/UserContext";
+import CurrentRoomContext from "../context/CurrentRoomContext";
 import "./ChatApp.css";
 
 const ChatApp = () => {
+  const [user, setUser] = useState(null);
+  const [currentRoom, setCurrentRoom] = useState(null);
   return (
     <div className="ChatApp">
-      <SidePanel />
-      <ChatPanel />
+      <UserContext.Provider value={{ user, setUser }}>
+        <CurrentRoomContext.Provider value={{ currentRoom, setCurrentRoom }}>
+          <SidePanel />
+          <ChatPanel />
+        </CurrentRoomContext.Provider>
+      </UserContext.Provider>
     </div>
   );
 };
