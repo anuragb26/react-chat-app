8adbd79cb2f221f26872ee1d3c207d0d382b2d01
Anurag Bajaj <anuragb26@gmail.com>
2020-03-26 05:40:22 +0530

    Feat:Show Initial Messages

--- src/components/ChatPanel.js
+++ src/components/ChatPanel.js
@@ -1,18 +1,43 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import firebase from "../config/firebase";
 import "./ChatPanel.css";
 
 const ChatPanel = () => {
+  const [messages, setMessages] = useState([]);
+  const messagesRef = useRef();
+  messagesRef.current = messages;
   const messagesRefFirebase = firebase.database().ref("messages");
+  const messageElements = (
+    <ul>
+      {messages.map((item, index) => (
+        <li key={index}>{item.text}</li>
+      ))}
+    </ul>
+  );
+  const addMessageListener = () => {
+    console.log("addMessageListener");
+    messagesRefFirebase.on("child_added", snap => {
+      console.log("snap", snap.val());
+      console.log("messages", messages);
+      setMessages([...messagesRef.current, snap.val()]);
+    });
+  };
+  const removeMessageListener = () => {
+    messagesRefFirebase.off();
+  };
+
+  useEffect(() => {
+    addMessageListener();
+    return () => {
+      removeMessageListener();
+    };
+  }, []);
+
   return (
     <div className="ChatPanel">
       <div>room1</div>
       <div style={{ flexGrow: 1, backgroundColor: "violet" }}>
-        <ul>
-          <li>Message 1</li>
-          <li>Message 1</li>
-          <li>Message 1</li>
-        </ul>
+        {messageElements}
       </div>
       <div style={{ display: "flex" }}>
         <input
