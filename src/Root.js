e37bacf989a9ed589de6b07daf0d5f344351bb0d
Anurag Bajaj <anuragb26@gmail.com>
2020-03-29 02:37:25 +0530

    Chore:Handle Auth Changed

--- src/Root.js
+++ src/Root.js
@@ -1,12 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import { Switch, Route } from "react-router-dom";
 import ChatApp from "./components/ChatApp";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 import UserContext from "./context/UserContext";
+import firebase from "./config/firebase";
 
 const Root = () => {
-  const [user, setUser] = useState(null);
+  const history = useHistory();
+  const [user, setUser] = useState({ displayName: "", uid: "", photoURL: "" });
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        const { displayName, uid, photoURL } = user;
+        setUser({ displayName, uid, photoURL });
+        history.push("/");
+        // navigate to /
+      } else {
+        history.push("/login");
+        // navigate to login
+      }
+    });
+  }, []);
+
   return (
     <UserContext.Provider value={{ user, setUser }}>
       <Switch>
